c7f15179337e9f3624411cbcc8469aa6ab60fb3b
MoeMoeFish <moemoefish@qq.com>
2023-02-01 20:14:09 +0800

    feat: q00004 solution2 run completed

--- src/q00004/mod.rs
+++ src/q00004/mod.rs
@@ -2,7 +2,7 @@
  * @Author: MoeMoeFish moemoefish@qq.com
  * @Date: 2023-01-10 11:04:19
  * @LastEditors: MoeMoeFish moemoefish@qq.com
- * @LastEditTime: 2023-01-31 14:03:45
+ * @LastEditTime: 2023-02-01 20:12:45
  * @Description: 
  */
 
@@ -13,17 +13,19 @@ pub(crate) fn demo() {
     let nums1 = vec![1,3];
     let nums2 = vec![1,3];
     println!("q0004 solution1: {}", solution1::Solution::find_median_sorted_arrays(nums1.clone(), nums2.clone()));
+    println!("q0004 solution2: {}", solution2::Solution::find_median_sorted_arrays(nums1.clone(), nums2.clone()));
 }
 
 #[cfg(test)]
 mod tests {
     use super::solution1;
+    use super::solution2;
 
     fn inner_test(nums1: &Vec<i32>, nums2: &Vec<i32>, answer: f64) {
         let ret = solution1::Solution::find_median_sorted_arrays(nums1.clone(), nums2.clone());
         assert_eq!(ret, answer);
 
-        let ret2 = solution1::Solution::find_median_sorted_arrays(nums1.clone(), nums2.clone());
+        let ret2 = solution2::Solution::find_median_sorted_arrays(nums1.clone(), nums2.clone());
         assert_eq!(ret2, answer);
     }
 
@@ -43,5 +45,15 @@ mod tests {
         let answer = 2.5;
 
         inner_test(&nums1, &nums2, answer);
+    }
+
+    #[test]
+    fn test03() {
+        let nums1 = vec![];
+        let nums2 = vec![1];
+        let answer = 1.0;
+
+        inner_test(&nums1, &nums2, answer);
+
     }
 }
--- src/q00004/solution2.rs
+++ src/q00004/solution2.rs
@@ -2,7 +2,7 @@
  * @Author: MoeMoeFish moemoefish@qq.com
  * @Date: 2023-01-31 13:49:42
  * @LastEditors: MoeMoeFish moemoefish@qq.com
- * @LastEditTime: 2023-01-31 14:16:48
+ * @LastEditTime: 2023-02-01 20:11:16
  * @Description: 在两个数组中分别定义两个点，根据中位数的定义有 i + j = (m - i) + (n - j)
  */
 
@@ -14,20 +14,39 @@ impl Solution {
         let n = nums2.len();
         let len = m + n;
 
-        if m < n {
+        if m > n {
             return Solution::find_median_sorted_arrays(nums2, nums1);
         }
 
-        let i = 0;
-        let j = (m + n) / 2 - i;
-
-        loop {
-            if (nums1[i] < nums2[j + 1]) && (nums2[j] < nums1[i + 1]) {
-                return (cmp::max(nums1[i], nums2[j]) + cmp::min(nums1[i + 1], nums2[j + 1])) as f64 / 2.0;
-            } 
+        if m == 0 {
+            if len % 2 == 0 {
+                return (nums2[n / 2 - 1] + nums2[n / 2]) as f64 / 2.0;
+            } else {
+                return nums2[n / 2] as f64;
+            }
         }
 
-        todo!("");
+        let mut left = 0;
+        let mut right = m;
+        loop {
+            let i = (left + right) / 2;
+            let j = (len + 1) / 2 - i;
+
+            if i > 0 && j < n && nums1[i - 1] > nums2[j] {
+                right = i - 1;
+            } else if i < m && j > 0 && nums2[j - 1] > nums1[i] {
+                left = i + 1;
+            } else {
+                let ret_left = if i == 0 { nums2[j - 1] } else if j == 0 { nums1[i - 1] } else { cmp::max(nums1[i - 1], nums2[j - 1]) };
+                let ret_right = if i == m { nums2[j] } else if j == n { nums1[i] } else { cmp::min(nums1[i], nums2[j]) };
+
+                if len % 2 == 1 {
+                    return ret_left as f64;
+                } else {
+                    return (ret_left + ret_right) as f64 / 2.0;
+                }
+            }
+        }
 
     }
 }
