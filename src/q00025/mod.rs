73e70edaae52dc981c7850c36ab08dbc76a79514
moemoefish <moemoefish@qq.com>
2023-04-06 20:22:27 +0800

    feat: daily

--- src/q00025/mod.rs
+++ src/q00025/mod.rs
@@ -1,8 +1,8 @@
 /*
  * @Author: MoeMoeFish moemoefish@qq.com
  * @Date: 2023-04-05 10:16:33
- * @LastEditors: MoeMoeFish moemoefish@qq.com
- * @LastEditTime: 2023-04-05 10:25:13
+ * @LastEditors: moemoefish moemoefish@qq.com
+ * @LastEditTime: 2023-04-06 13:21:04
  * @Description: q00025
  */
 use crate::libs::list_node;
@@ -10,12 +10,14 @@ use crate::libs::list_node;
 mod solution1;
 
 pub (crate) fn demo() {
-    // let input = vec![vec![1,4,5],vec![1,3,4],vec![2,6]];
-
     let input = vec![1,2,3,4,5];
     let k = 2;
 
     let ret1 = solution1::Solution::reverse_k_group(list_node::convert_vec_to_linked_list(input), k);
 
     println!("q00025 solution1. {:?}", ret1);
+}
+
+#[cfg(test)]
+mod tests {
 }
--- src/q00025/solution1.rs
+++ src/q00025/solution1.rs
@@ -1,18 +1,74 @@
 /*
  * @Author: moemoefish moemoefish@qq.com
  * @Date: 2023-04-03 19:51:20
- * @LastEditors: MoeMoeFish moemoefish@qq.com
- * @LastEditTime: 2023-04-05 10:23:45
+ * @LastEditors: moemoefish moemoefish@qq.com
+ * @LastEditTime: 2023-04-06 20:19:56
  * @Description: q00025 solution1
  */
 
-use crate::libs::list_node;
 use crate::libs::list_node::ListNode;
-
 
 impl Solution {
     pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        todo!();
+        if k == 1 {
+            return head;
+        }
+
+        Solution::reverse_inner(head, k)
+    }
+
+    fn reverse_inner(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+
+        let mut ret_head = Some(Box::new(ListNode::new(0)));
+        let mut last = &mut ret_head;
+
+        // let mut head_mut = &mut head;
+        let mut group_curr = &mut head;
+        let mut next_group = &head;
+
+
+        loop {
+            let mut i = k;
+            let mut need_reverse = true;
+            let mut group_head = Some(Box::new(ListNode::new(0)));
+
+            while i >= 0 {
+                if let Some(n) = next_group {
+                    next_group = &n.next;
+                    i -= 1;
+                } else {
+                    need_reverse = false;
+                }
+            }
+
+            if need_reverse {
+                let mut iter = group_curr;
+
+                loop {
+                    if iter == next_group {
+                        break;
+                    }
+
+                    if let Some(n) = iter.as_mut() {
+                        let mut b = n.next.take();
+                        if let Some(h) = group_head {
+                            if h.next.is_some() {
+                                // let t = h.next.take();
+                                // h.next = Some(Box::new(ListNode::new(0)));
+                            }
+                        }
+                    }
+
+                }
+                todo!();
+                break;
+            } else {
+                todo!();
+            }
+            
+        }
+
+        ret_head.unwrap().next
     }
 }
 
