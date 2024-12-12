ecdd8658340236d8a07c5e03ee334c7a97d2d000
moemoefish <moemoefish@qq.com>
2024-12-13 00:06:49 +0800

    feat: temp

--- src/q00094/solution2r.rs
+++ src/q00094/solution2r.rs
@@ -1,5 +1,4 @@
 use crate::utils::tree_node::TreeNode;
-use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -24,7 +23,43 @@ impl Solution {
                         break;
                     }
 
-                    curr = rc.as_ref().borrow().right.clone();
+                    curr = node.right.clone();
+                } else {
+                    let mut pre = node.left.clone();
+                    if let Some(ref leftc) = node.left.clone() {
+                        let left_node = leftc.as_ref().borrow();
+
+                        if left_node.right.is_some() {
+                            loop {
+                                if let Some(ref prec) = pre.clone() {
+                                    let pre_node = prec.as_ref().borrow();
+                                    if pre_node.right.is_none() || pre_node.right == curr {
+                                        break;
+                                    }
+                                    pre = pre_node.right.clone();
+                                }
+                            }
+                        }
+
+                        if let Some(ref mut prec) = pre.clone() {
+                            let mut pre_node = prec.as_ref().borrow_mut();
+
+                            if pre_node.right == curr {
+                                ret.push(pre_node.val);
+                                pre_node.right = None;
+
+
+                                if node.right.is_none() {
+                                    break;
+                                }
+
+                                curr = node.right.clone();
+                            } else {
+                                pre_node.right = curr;
+                                curr = node.left.clone();
+                            }
+                        }
+                    }
                 }
                 
             }
