daded8c7d266d2e2eeabc1bac4e0401b5c9ef667
moemoefish <moemoefish@qq.com>
2025-02-13 13:46:34 +0800

    daily

--- src/q00094/solution2r.rs
+++ src/q00094/solution2r.rs
@@ -1,70 +1,49 @@
-use crate::utils::tree_node::TreeNode;
 use std::cell::RefCell;
+use std::ops::Deref;
 use std::rc::Rc;
+
+use crate::utils::tree_node::TreeNode;
+
 
 impl Solution {
     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-        let mut ret: Vec<i32> = vec![];
+        let mut result: Vec<i32> = vec![];
+
+        if root.is_none() {
+            return result;
+        }
 
         let mut curr = root;
 
         loop {
-            if curr.is_none() {
+            if let Some(curr_node) = curr.clone() {
+                let inner_node = curr_node.as_ref().borrow();
+
+                if inner_node.left.is_none() {
+                    result.push(inner_node.val);
+
+                    if inner_node.right.is_none() {
+                        break;
+                    } else {
+                        curr = inner_node.right.clone();
+                        continue;
+                    }
+                } else {
+                    let mut pre = curr_node.clone().as_ref().borrow().left; 
+
+                    loop {
+                        if let Some(pre_node) = pre.clone() {
+                            
+                        }
+                    }
+
+                }
+            } else {
                 break;
             }
-
-            if let Some(ref rc) = curr.clone() {
-                let node  = rc.as_ref().borrow();
-
-                if node.left.is_none() {
-                    ret.push(node.val);
-
-                    if node.right.is_none() {
-                        break;
-                    }
-
-                    curr = node.right.clone();
-                } else {
-                    let mut pre = node.left.clone();
-                    if let Some(ref leftc) = node.left.clone() {
-                        let left_node = leftc.as_ref().borrow();
-
-                        if left_node.right.is_some() {
-                            loop {
-                                if let Some(ref prec) = pre.clone() {
-                                    let pre_node = prec.as_ref().borrow();
-                                    if pre_node.right.is_none() || pre_node.right == curr {
-                                        break;
-                                    }
-                                    pre = pre_node.right.clone();
-                                }
-                            }
-                        }
-
-                        if let Some(ref mut prec) = pre.clone() {
-                            let mut pre_node = prec.as_ref().borrow_mut();
-
-                            if pre_node.right == curr {
-                                ret.push(pre_node.val);
-                                pre_node.right = None;
-
-
-                                if node.right.is_none() {
-                                    break;
-                                }
-
-                                curr = node.right.clone();
-                            } else {
-                                pre_node.right = curr;
-                                curr = node.left.clone();
-                            }
-                        }
-                    }
-                }
-                
-            }
         }
-        return ret;
+
+        result
     }
 }
 
