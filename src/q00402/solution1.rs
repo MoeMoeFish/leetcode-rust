e4822a89bed8876a07b05f935a372f235b3c62dc
moemoefish <moemoefish@qq.com>
2025-03-25 18:31:18 +0800

    feat: q00402

--- src/q00402/solution1.rs
+++ src/q00402/solution1.rs
@@ -2,9 +2,24 @@ impl Solution {
     pub fn remove_kdigits(num: String, k: i32) -> String {
         let mut result = String::new();
         let mut left = k;
-        let mut index = 0;
+        let mut index: i32 = 0;
+        let len = num.len() as i32;
 
-        
+        for char in num.chars() {
+            if left == 0 {
+                break;
+            }
+            if (len - index) <= left {
+                result.push(char);
+                left = left - 1;
+            } else {
+                
+            }
+
+            index += 1;
+        }
+
+        result
     }
 }
 
